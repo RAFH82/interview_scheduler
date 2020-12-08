c408fb860d26b427dc097845b2c8efb9084b02a9
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-12-08 12:27:35 -0800

    Completed day 1 exercises

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 import 'components/Button.scss';
 
+const classNames = require('classnames');
+
 export default function Button(props) {
-	return <></>;
+	let buttonClass = classNames('button', { 'button--confirm': props.confirm, 'button--danger': props.danger });
+
+	return (
+		<button 
+		disabled={props.disabled} 
+		className={buttonClass} 
+		onClick={props.onClick}
+		>
+		{props.children}
+		</button>
+	);
 }

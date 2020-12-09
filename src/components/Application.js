85afae84e0b0234f4eeb6da04af9aef556c45cdc
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-12-08 21:46:10 -0800

    Completed exercise w7d2

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
 import 'components/Application.scss';
+
 import DayList from 'components/DayList';
+import Appointment from 'components/Appointment';
 
 const days = [
 	{
@@ -21,9 +23,73 @@ const days = [
 	},
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Arnold Schwarzenagger",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Timothy Tiddles",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sally Bojangles",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
 
 	const [day, setDay] = useState('Monday');
+
+	const appsList = appointments.map( appointment => {
+		return (
+			<Appointment
+			key={appointment.id }
+			{... appointment}
+			/>
+		);
+	})
 
 	return (
 		<main className="layout">
@@ -40,7 +106,8 @@ export default function Application(props) {
 				<img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
 			</section>
 			<section className="schedule">
-				{/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+				{appsList}
+				<Appointment key="last" time="5pm" />
 			</section>
 		</main>
 	);

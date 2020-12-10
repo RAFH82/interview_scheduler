36cc857bd86b401975d944134cbc9bea73a7601e
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-12-10 14:46:27 -0800

    Completed Building a hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,38 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
 
-  const [mode, setMode] = useState(initial)
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) {
+  const transition = (newMode, replace = false) => {
+
+    const newHistory = [...history]
+
+    if (replace) {
+      newHistory.pop()
+    }
+
+    newHistory.push(newMode)
+
     setMode(newMode);
+
+    setHistory(newHistory);
   }
 
-  return { mode, transition }
+  const back = () => {
+
+    const newHistory = [...history]
+
+    if (newHistory.length < 2) {
+      return;
+    }
+    
+    newHistory.pop();
+
+    setMode(newHistory[newHistory.length - 1]);
+
+    setHistory(newHistory);
+  };
+
+  return { mode, transition, back }
 };

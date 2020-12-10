759bd2b40b9a4915122dd79cb618f066feec5c23
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-12-09 21:10:34 -0800

    Completed "Retrieving Interviewer Data" exercise

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,14 +6,16 @@ import 'components/Application.scss';
 
 import DayList from 'components/DayList';
 import Appointment from 'components/Appointment';
-import getAppointmentsForDay from 'helpers/selectors';
+import { getAppointmentsForDay } from 'helpers/selectors';
+import { getInterview } from 'helpers/selectors';
 
 export default function Application(props) {  
 
   const [state, setState] = useState({ 
     day: 'Monday',
     days: [],
-    appointments: {}  
+    appointments: {},
+    interviewers: {} 
   });
 
   useEffect(() => {
@@ -30,20 +32,25 @@ export default function Application(props) {
         ...prev, 
         days: all[0].data, 
         appointments: all[1].data, 
-        interviewers: all[2].data, 
+        interviewers: all[2].data,
       }));
     })
   },[])
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
-
   const setDay = day => setState({ ...state, day });
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
 	const appsList = dailyAppointments.map( appointment => {
+
+    const interview = getInterview(state, appointment.interview);
+
 		return (
 			<Appointment
-			key={appointment.id }
-			{... appointment}
+			key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
 			/>
 		);
 	})

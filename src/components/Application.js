8cf1d500c3ebfa6496d0538e003c30bd3c495ab6
Ryan Hrechka <rafh@Ryans-MacBook-Pro.local>
2020-12-09 19:17:31 -0800

    updated some formatting

--- src/components/Application.js
+++ src/components/Application.js
@@ -31,10 +31,13 @@ export default function Application(props) {
       axios.get(second),
       axios.get(third),
       ]).then( all => {
-        
-      // console.log(all[0], all[1], all[2])
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-
+      
+      setState(prev => ({
+        ...prev, 
+        days: all[0].data, 
+        appointments: all[1].data, 
+        interviewers: all[2].data, 
+      }));
     })
   },[])
 

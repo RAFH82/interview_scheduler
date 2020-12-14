dbd740e16998a895e74495c2c360c19e1be1b444
RAFH82 <hrechkar@gmail.com>
2020-12-14 12:30:35 -0800

    Removed nested blocks (the {}) in line16, 20

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,49 +2,54 @@ import React, { useState } from 'react';
 import Button from 'components/Button';
 import InterviewerList from 'components/InterviewerList';
 
-export default function Form(props) { 
+export default function Form(props) {
+	const [name, setName] = useState(props.name || '');
+	const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
-  const [name, setName] = useState(props.name || '');
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+	const reset = () => {
+		setName('');
+		setInterviewer(null);
+	};
 
-  const reset = () => { 
-    setName('')
-    setInterviewer(null)
-  }
+	const cancel = () => {
+		props.onCancel(reset());
+	};
 
-  const cancel = () => {
-    {props.onCancel(reset())}
-  }
+	const save = () => {
+		props.onSave(name, interviewer);
+	};
 
-  const save = () => {
-    {props.onSave(name, interviewer)}
-  }
-
-  return (
-    <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-          <input
-            className="appointment__create-input text--semi-bold"
-            name="name"
-            type="text"
-            placeholder="Enter Student Name"
-            value={name}
-            onChange={event => {setName(event.target.value)}}
-          />
-        </form>
-        <InterviewerList 
-          interviewers={props.interviewers} 
-          value={interviewer} 
-          onChange={setInterviewer} 
-        />
-      </section>
-      <section className="appointment__card-right">
-        <section className="appointment__actions">
-          <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={save} confirm>Save</Button>
-        </section>
-      </section>
-    </main>
-  );
-};
+	return (
+		<main className="appointment__card appointment__card--create">
+			<section className="appointment__card-left">
+				<form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+					<input
+						className="appointment__create-input text--semi-bold"
+						name="name"
+						type="text"
+						placeholder="Enter Student Name"
+						value={name}
+						onChange={(event) => {
+							setName(event.target.value);
+						}}
+					/>
+				</form>
+				<InterviewerList
+					interviewers={props.interviewers}
+					value={interviewer}
+					onChange={setInterviewer}
+				/>
+			</section>
+			<section className="appointment__card-right">
+				<section className="appointment__actions">
+					<Button onClick={cancel} danger>
+						Cancel
+					</Button>
+					<Button onClick={save} confirm>
+						Save
+					</Button>
+				</section>
+			</section>
+		</main>
+	);
+}
